86a363cb00e98fd4154a5791761b97b8f189745f
alperenkucukozkan <alperenkucukozkan@gmail.com>
2025-09-25 19:29:57 +0300

    final commit

--- js/task-4.js
+++ js/task-4.js
@@ -2,7 +2,7 @@ const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    const { email, password } = event.elements;
+    const { email, password } = event.target.elements;
     if (email.value.trim() === "" || password.value.trim() === "") {
         alert("All form fields must be filled!");
         return;
